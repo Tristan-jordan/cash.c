f2517470d30cb4bbe00929d108b1e601e36e4aa8
Tristan-jordan <Tristan-jordan@users.noreply.github.com>
2020-09-29 18:45:20 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -1,45 +1,50 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 int main(void)
 {
-    float change, cn25 = 0.25, cn10 = 0.10, cn5 = 0.05, cn1 = 0.01; 
-    int cnusd;
+    float cash;  
+    int cnusd, change, cn25 = 25, cn10 = 10, cn5 = 5, cn1 = 1;
+    cnusd = 0;
 
     do
-    change = get_float("Change owed?: $");
-    while (change < 0.00);
-
+    cash = get_float("Change owed?: $");
+    while (cash < 0);
+ 
+    change = round(cash * 100);
+    
     do
-    if (change >= 0.25)
+    if (change >= 25)
         {
             change = change - cn25;
-            cnusd++;          
+            cnusd++; 
         }
-     while (change >= 0.25);
+     while (change >= 25);
 
     do
-    if (change < 0.25 && change > 0.10)
+    if (change < 25 && change >= 10)
         {
             change = change - cn10;
             cnusd++;
+
         }
-     while (change >= 0.10);   
+     while (change > 10);   
      
-        if (change > 0.05 && change < 0.10)
+        if (change >= 5 && change < 10)
         {
             change = change - cn5;
             cnusd++;
         }   
 
         do
-        if (change > 0.00 && change < 0.05)
+        if (change >= 1 && change < 5)
         {
             change = change - cn1;
             cnusd++;
         }
-        while (change > 0.00);
+        while (change >= 1);
     
-printf("%i\n", cnusd);
- 
+printf("Coins used: %.2d\n", cnusd);
+    
 }
